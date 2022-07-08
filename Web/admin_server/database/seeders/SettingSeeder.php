587550548b24ccb9c6fd1d1277845ea272c9cd19
Hein Thu Ya <heinthuya12@gmail.com>
2022-07-08 10:57:49 +0630

    fix

---
+++ Web/admin_server/database/seeders/SettingSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    protected $settings = [
+        [
+            'key'                       =>  'app_name',
+            'value'                     =>  'Moon Light VPN',
+        ],
+        [
+            'key'                       =>  'app_description',
+            'value'                     =>  ''
+        ],
+        [
+            'key'                       =>  'app_logo',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'phone_number',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'email',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'address',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'facebook',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'instagram',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'twitter',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'linkedin',
+            'value'                     =>  '',
+        ],
+    ];
+
+    public function run()
+    {
+        foreach ($this->settings as $index => $setting)
+        {
+            $result = Setting::create($setting);
+            if (!$result) {
+                $this->command->info("Insert failed at record $index.");
+                return;
+            }
+        }
+        $this->command->info('Inserted '.count($this->settings). ' records');
+    }
+}
---
+++ Web/admin_server/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\SettingController;
+
+Route::get('/', function () {
+    return redirect()->route('index');
+});
+
+
+Route::group(["prefix" => "admin"], function () {
+    Auth::routes();
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::post('/update', [AdminController::class, 'updateServer'])->name('update_server');
+    Route::post('/update/switch', [AdminController::class, 'switch'])->name('switch_status');
+    Route::post('/add', [AdminController::class, 'addServer'])->name('add_server');
+    Route::post('/delete', [AdminController::class, 'deleteServer'])->name('delete_server');
+    Route::get('/setting', [SettingController::class, 'index'])->name('setting');
+    Route::post('/setting', [SettingController::class, 'update'])->name('setting.update');
+});
